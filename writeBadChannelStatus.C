227419394e666295d8edcf5e25db52c5bc8230c8
cbianchi <cbianchi@cern.ch>
2016-07-12 11:37:28 +0200

    Working on this macro not yet working

--- writeBadChannelStatus.C
+++ writeBadChannelStatus.C
@@ -55,74 +55,51 @@
 #endif
 
 void etaphi(){ 
-  Double_t eta, phi;
-  Int_t  absId=5430;
-  AliEMCALGeometry *geo = new AliEMCALGeometry() ;
-geo->EtaPhiFromIndex(absId, eta, phi);
- cout<<eta<<phi<<endl;
-
+	Double_t eta, phi;
+	Int_t  absId=5430;
+	AliEMCALGeometry *geo = new AliEMCALGeometry() ;
+	geo->EtaPhiFromIndex(absId, eta, phi);
+	cout<<eta<<phi<<endl;
+	
 }
 
-void writeBadChannelStatus(Int_t istat){
-
-
-  //   fdata = fopen("/scratch/alicehp2/germain/PedestalDB/LHC11aLE-period2.txt","r");
-
-  //    fdata = fopen("/scratch/alicehp2/germain/QA/LHC11c/LHC11crun153353-153560And154763-154796.txtWithComments","r");
-  //  fdata = fopen("/scratch/alicehp2/germain/QA/LHC11c/LHC11crun153570-154480.txtWithComments","r");
-  // fdata = fopen("/scratch/alicehp2/germain/QA/LHC11b/LHC11bFull4pass2.txt","r");
-  // fdata = fopen("/scratch/alicehp2/germain/PedestalDB/LHC11dBadChannels.txt","r");
-
-  //  fdata = fopen("/scratch/alicehp2/germain/PedestalDB/LHC12a176326-177295.txt","r");
-
-  //   fdata = fopen("/scratch/alicehp2/germain/PedestalDB/BadCellCandidateLHC12ccpass1FullEMCAL.txt","r");
-  //    fdata = fopen("/scratch/alicehp2/germain/PedestalDB/BadCellsLHC12efromcpass1.txt","r");
-  //fdata = fopen("/scratch/alicehp2/germain/PedestalDB/LHC12dvpass1run185456to185784.txt","r");
-  //fdata = fopen("/scratch/alicehp2/germain/PedestalDB/LHC12hcpass1.txt","r");
-  //  fdata = fopen("/scratch/alicehp2/germain/PedestalDB/LHC11hFinal.txt","r");
-  //    fdata = fopen("/scratch/alicehp2/germain/PedestalDB/BCLHC11a2.txt","r");      
-  // fdata = fopen("/scratch/alicehp2/germain/PedestalDB/BC2013-3.txt","r");
-  // fdata = fopen("/scratch/alicehp2/germain/PedestalDB/additionnalBC2013New.txt","r");
-  // fdata = fopen("/scratch/alicehp2/germain/PedestalDB/dead197348-197388.txt","r");
-  // fdata = fopen("/scratch/alicehp2/germain/PedestalDB/addBCLHC13b-fFromMarta.txt","r");
-  //    fdata = fopen("/scratch/alicehp2/germain/PedestalDB/2012FromDaniel/new/LHC12badcellsDaniel/LHC12i-SortedByRun.log","r");
-  //      fdata = fopen("/scratch/alicehp2/germain/PedestalDB/2012FromDaniel/deadLHC12i193750-193766.txt","r");
-  //  fdata = fopen("/scratch/alicehp2/germain/QANew2/BadChannelNew/ResultsLHC15omuon_calopass1allrunsDEAD.txt","r");
-  //  fdata = fopen("/scratch/alicehp2/germain/QANew2/BadChannelNew/LHC15oBadINT7.txt","r");
-  fdata = fopen("/scratch/alicehp2/germain/QANew2/BadChannelNew/LHC15jMBDEAD.txt","r");
-  //fdata = fopen("/scratch/alicehp2/germain/QANew2/BadChannelNew/ResultsLHC15iallruns4sigTestallenergyWARM.txt","r");
-  // fdata = fopen("/scratch/alicehp2/germain/PedestalDB/2012FromDaniel/LHC12cspecialRuns182730-182744.txt","r");
-
-const char* geoType="EMCAL_COMPLETE12SMV1_DCAL_8SM";
-//const char* geoType="EMCAL_COMPLETE12SMV1";
-AliEMCALGeometry* geom = new AliEMCALGeometry(geoType,"EMCAL");
-//cout<<"AliEMCALGeoUtils('"<<geoType<< ",'EMCAL') !!"<<endl;
-//cout <<" how many SMs?:"<<(geom->GetEMCGeometry())->GetNumberOfSuperModules()<< endl;
-
- Int_t absId, status;
-
- Int_t p, q,r;
-  Int_t ncols;
-  Int_t nlines = 0 ;
-
-
-  while (1){
-    ncols = fscanf(fdata,"%d",&p);
-    if (ncols< 0) break;
-    absId=p;
+void writeBadChannelStatus(Int_t istat, TString pathinput = "LHC15omuon_calopass1BC_SummaryResults_V0.txt", const char* geoType="EMCAL_COMPLETE12SMV1_DCAL_8SM"){
  
-    // run=q;
-    status=istat;
-
-
-Int_t nSupMod, nModule, nIphi, nIeta;
-  Int_t iphi, ieta,kk;
+  ifstream fdata(pathinput);
+ 
   
-  // kk is the AbsId.
- geom->GetCellIndex(absId,  nSupMod, nModule, nIphi, nIeta);
- geom->GetCellPhiEtaIndexInSModule(nSupMod,nModule,nIphi,nIeta, iphi,ieta);
- //cout << absId <<  nSupMod  << ieta << iphi << status <<endl;
- printf("%d \t %d \t %d \t %d \t %d \n",absId,nSupMod,ieta,iphi,status);
- // printf("%d \t %d %d %d %d \n",absId,nSupMod,ieta,iphi,status);
-}
+  //const char* geoType="EMCAL_COMPLETE12SMV1";
+  AliEMCALGeometry* geom = new AliEMCALGeometry(geoType,"EMCAL");
+  //cout<<"AliEMCALGeoUtils('"<<geoType<< ",'EMCAL') !!"<<endl;
+  //cout <<" how many SMs?:"<<(geom->GetEMCGeometry())->GetNumberOfSuperModules()<< endl;
+  
+  Int_t absId, status;
+  
+  Int_t p = -1;//, q,r;
+  Int_t ncols;
+  //Int_t nlines = 0 ;
+  string line;
+  
+  while (fdata){
+  	  getline(fdata, line);
+  	  ncols = atoi(line.c_str());
+  	  Printf("Line = %s, number = %d", line.data(), ncols);
+  	  // = fscanf(fdata,"%d",&p);
+  	  if (ncols< 0) break;
+  	  absId=p;
+  	  
+  	  // run=q;
+  	  status=istat;
+  	  
+  	  
+  	  Int_t nSupMod, nModule, nIphi, nIeta;
+  	  Int_t iphi, ieta,kk;
+  	  
+  	  // kk is the AbsId.
+  	  geom->GetCellIndex(absId,  nSupMod, nModule, nIphi, nIeta);
+  	  geom->GetCellPhiEtaIndexInSModule(nSupMod,nModule,nIphi,nIeta, iphi,ieta);
+  	  //cout << absId <<  nSupMod  << ieta << iphi << status <<endl;
+  	  printf("%d \t %d \t %d \t %d \t %d \n",absId,nSupMod,ieta,iphi,status);
+  	  // printf("%d \t %d %d %d %d \n",absId,nSupMod,ieta,iphi,status);
+  }
 }
