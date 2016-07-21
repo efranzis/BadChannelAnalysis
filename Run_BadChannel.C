1cde6fe2f259b70c786fb88741b7d262dd08035d
efranzis <eliane.epple@yale.edu>
2016-07-21 09:33:21 -0400

    Included new QA stuff

--- Run_BadChannel.C
+++ Run_BadChannel.C
@@ -37,6 +37,9 @@ void Run_BadChannel(TString period = "LHC15f", TString pass = "pass2", TString t
 {
 	AliAnaCaloChannelAnalysis* Analysis=new AliAnaCaloChannelAnalysis(period,pass,trigger,runNum,workDir,listName);
 	Analysis->SetExternalMergedFile(externalFile);
+	Analysis->SetQAChecks(0);  //1=Perform QA checks
+	Analysis->SetNTrial(4);  //Perform QA checks
+	Analysis->MaskEdgesSM(1);  //Mask the supermodule edges as bad
 
 	if(trigger=="default"||trigger=="INT7"||trigger=="DMC7"||trigger=="AnyINTnoBC")
 	{
