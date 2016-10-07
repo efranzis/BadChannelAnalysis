4f7c817be4cc8a1f09a0176012ab61543e8127b7
efranzis <eliane.epple@yale.edu>
2016-10-07 16:49:22 -0400

    Added a TestOADB function to check the correctness of whats submitted by Marcel

--- Run_BadChannel.C
+++ Run_BadChannel.C
@@ -25,21 +25,21 @@
 #include "AliEMCALGeometry.h"          //include when compile
 #include "AliCalorimeterUtils.h"       //include when compile
 #include "AliAODEvent.h"               //include when compile
+#include "AliOADBContainer.h"          //include when compile
 */
 //________________________________________________________________________
-void Run_BadChannel(TString period = "LHC15n", TString train = "Train_603", TString trigger= "AnyINTnoBC", Int_t runNum= 244411, TString externalFile= "",TString workDir=".", TString listName="runList.txt")
+void Run_BadChannel(TString period = "LHC15n", TString train = "Train_603", TString trigger= "AnyINTnoBC", Int_t runNum= 244411, TString externalFile= "",TString listName="runList.txt",TString workDir=".")
 {
 	AliAnaCaloChannelAnalysis* Analysis;
 	//..If you do the analysis run by run - this might be helpful
 	//Analysis=new AliAnaCaloChannelAnalysis(period,train,trigger,runNum,runNum,workDir,listName);
 
 	//..If you do it with merged files use this
-	Analysis=new AliAnaCaloChannelAnalysis(period,train,trigger,runNum,2,workDir,listName);
+	Analysis=new AliAnaCaloChannelAnalysis(period,train,trigger,runNum,100,workDir,listName);
 
 	//..Settings
 	Analysis->SetExternalMergedFile(externalFile);
-	Analysis->SetQAChecks(0);  //1=Perform QA checks
-	//Analysis->MaskEdgesSM(1);  //Mask the supermodule edges as bad - not yet implemented
+	//Analysis->SetQAChecks(1);  //1=Perform QA checks - takes a long time! Prints all good cells for cross check
 
 	//. . . . . . . . . . . . . . . . . . . . . . . .
 	//. . Add different period analyses
@@ -54,8 +54,13 @@ void Run_BadChannel(TString period = "LHC15n", TString train = "Train_603", TStr
 	//..If there is enough statistic add also these:
 	Analysis->AddPeriodAnalysis(2, 4.,1.0,4.0); // hit/event in range Emin Emax
 	Analysis->AddPeriodAnalysis(1, 6.,1.0,4.0); // mean energy in range Emin Emax
-	Analysis->AddPeriodAnalysis(2, 4.,1.0,10.0);// hit/event in range Emin Emax
-	Analysis->AddPeriodAnalysis(1, 5.,1.0,10.0);// energy/hit in range Emin Emax
+//	Analysis->AddPeriodAnalysis(2, 4.,1.0,10.0);// hit/event in range Emin Emax
+//	Analysis->AddPeriodAnalysis(1, 5.,1.0,10.0);// energy/hit in range Emin Emax
+	Analysis->AddPeriodAnalysis(2, 5.,3.0,10.0);// (PbPb extra range) hit/event in range Emin Emax
+	Analysis->AddPeriodAnalysis(1, 5.,3.0,10.0);// (PbPb extra range) energy/hit in range Emin Emax
+
+///*test time stuff*/	Analysis->AddPeriodAnalysis(3, 6,-20,+20);// energy/hit in range Emin Emax
+
 
 	//..Start the bad channel analysis
 	Analysis->Run();
@@ -96,74 +101,121 @@ void Get_RowCollumnID(Int_t runNum= 244411,Int_t inputCellID=-1,Int_t inputRow=-
 	cout<<""<<endl;
 	cout<<"...................................................."<<endl;
 }
-
 //________________________________________________________________________
-void CheckListDeadChannels(TString qaoutputPath = "/data/Work/EMCAL/BadChannels/LHC16h/510muon_caloLego/", TString runlist = "/data/Work/EMCAL/BadChannels/LHC16h/rulListLHC16h_EMCGood_Fieldp30.txt", TString listname = "CaloQA_AnyINT", TString hname = "EMCAL_hAmpId")
+void Test_OADB(TString period="LHC15n",Int_t trainNo=603,Int_t version=5,Int_t runNumber=244411)
 {
+	//......................................................
+	// Test if the file committed to OADB is correct
+	//......................................................
+	Int_t nSM = 20;
+	TH1C *h[20];
+    Int_t cellColumnAbs,cellRowAbs,trash,cellID;
 
-	// current list of dead cells from Martin
-	const Int_t ndeadSM4 = 32;
-	Int_t listDeadSM4[ndeadSM4] = {4833, 4832, 4881, 4880, 4835, 4834, 4883, 4882, 4837, 4836, 4885, 4884, 4839, 4838, 4887, 4886, 4841, 4840, 4889, 4888, 4843, 4842, 4891, 4890, 4845, 4844, 4893, 4892, 4847, 4846, 4895, 4894};
+	//......................................................
+	//..Get the OADB information
+	TString fBasePath="/Users/Eliane/Software/alice/sw/osx_x86-64/AliPhysics/0-1/OADB/EMCAL";
+	AliOADBContainer *cont=new AliOADBContainer("");
+	cont->InitFromFile(Form("%s/EMCALBadChannels.root",fBasePath.Data()),"AliEMCALBadChannels");
 
-	const Int_t ndeadSM7 = 32;
-	Int_t listDeadSM7[ndeadSM7] = {8481, 8480, 8529, 8528, 8483, 8482, 8531, 8530, 8485, 8484, 8533, 8532, 8487, 8486, 8535, 8534, 8489, 8488, 8537, 8536, 8491, 8490, 8539, 8538, 8493, 8492, 8541, 8540, 8495, 8494, 8543, 8542};
+	//......................................................
+    //..Get the .root file with the original histogram to compare if they coincide
+	TString path        = Form("/Users/Eliane/Software/BadChannelAnalysis/AnalysisOutput/%s/Version%d",period.Data(),version);
+	TString rootFileName= Form("Train_%dAnyINTnoBC_Histograms_V%d.root",trainNo,version);
+	TFile* outputRoot   = TFile::Open(Form("%s/%s",path.Data(),rootFileName.Data()));
 
-	ifstream read(runlist.Data());
-	Int_t nruns = 0;
-	TString currentRun = "";
-	while(read){
-		read>> currentRun;
-		Printf("Run %s", currentRun.Data());
+	TH2F* h2DChannelMap_FlagBad=(TH2F*)outputRoot->Get("2DChannelMap_Flag2");
+	TH2F* h2DChannelMap_FlagDead=(TH2F*)outputRoot->Get("2DChannelMap_Flag1");
 
-		TFile *fin = new TFile(Form("%s%s.root", qaoutputPath.Data(), currentRun.Data()));
+	/*cout<<"Open root file: "<<outputRoot->GetName()<<endl;
+	if(h2DChannelMap_FlagBad)h2DChannelMap_FlagBad->SetName("BadMapFromFile");
+	else cout<<"No valid file found"<<endl;*/
+	//......................................................
+	//..Initialize EMCal/DCal geometry
+	AliCalorimeterUtils* fCaloUtils = new AliCalorimeterUtils();
+	//..Create a dummy event for the CaloUtils
+	AliAODEvent* aod = new AliAODEvent();
+	fCaloUtils->SetRunNumber(runNumber);
+	fCaloUtils->AccessGeometry(aod);
+	AliEMCALGeometry * geom = fCaloUtils->GetEMCALGeometry();
 
-		if(!fin->IsOpen()){
-			Printf("%s%s.root not found", qaoutputPath.Data(), currentRun.Data());
-			continue;
-		}
+	//.......................................................
+	//..build two dimensional histogram with values row vs. column
+	//..with info from OADB
+	Int_t fNMaxCols    = 48;  //eta direction
+	Int_t fNMaxRows    = 24;  //phi direction
 
-		TDirectoryFile *dir = (TDirectoryFile*)fin->Get(listname);
-		if(!dir){
-			Printf("%s not found", listname.Data());
-			fin->ls();
-			continue;
-		}
+	Int_t fNMaxColsAbs = 2*fNMaxCols;
+	Int_t fNMaxRowsAbs = Int_t (nSM/2)*fNMaxRows; //multiply by number of supermodules
+	TString histoName;
+	histoName = Form("2DChannelMap_Flag_Bad");
+	TH2F *plot2D_Bad_OADB = new TH2F(histoName,histoName,fNMaxColsAbs+1,-0.5,fNMaxColsAbs+0.5, fNMaxRowsAbs+1,-0.5,fNMaxRowsAbs+0.5);
+///*OADB  looks like Marcels figures*/	TH2F *plot2D_Bad_OADB = new TH2F(histoName,histoName,fNMaxColsAbs,0,fNMaxColsAbs, fNMaxRowsAbs,0,fNMaxRowsAbs);
+	plot2D_Bad_OADB->GetXaxis()->SetTitle("cell column (#eta direction)");
+	plot2D_Bad_OADB->GetYaxis()->SetTitle("cell row (#phi direction)");
+	histoName = Form("2DChannelMap_Flag_Dead");
+	TH2F *plot2D_Dead_OADB = new TH2F(histoName,histoName,fNMaxColsAbs+1,-0.5,fNMaxColsAbs+0.5, fNMaxRowsAbs+1,-0.5,fNMaxRowsAbs+0.5);
+///*OADB looks like Marcels figures*/	TH2F *plot2D_Dead_OADB = new TH2F(histoName,histoName,fNMaxColsAbs,0,fNMaxColsAbs, fNMaxRowsAbs,0,fNMaxRowsAbs);
+	plot2D_Dead_OADB->GetXaxis()->SetTitle("cell column (#eta direction)");
+	plot2D_Dead_OADB->GetYaxis()->SetTitle("cell row (#phi direction)");
 
-		TList *list = (TList*)dir->Get(listname);
-		if(!list){
-			Printf("%s not found", listname.Data());
-			dir->ls();
-			continue;
-		}
+	//.......................................................
+	//.. Read the Bad Channel map from OADB
+	TObjArray *recal=(TObjArray*)cont->GetObject(runNumber);
+	//recal->ls();
+	TCanvas* C1 = new TCanvas();
+	C1->cd();
+	//nSM=1;
+	for(Int_t iSM = 0; iSM < nSM; iSM++)
+	{
+		h[iSM]=(TH1C *)recal->FindObject(Form("EMCALBadChannelMap_Mod%d",iSM));
+		h[iSM]->GetXaxis()->SetTitle("Column"); // tmp debug
+		h[iSM]->GetYaxis()->SetTitle("Row"); // tmp debug
+		h[iSM]->DrawCopy("colz"); // tmp debug
 
-		TH2D *hAmpCellId = (TH2D*)list->FindObject(hname);
-		if(!hAmpCellId){
-			Printf("hAmpCellId not found");
-			continue;
-		}
-
-		for(Int_t ideadsm4 = 0; ideadsm4 < ndeadSM4; ideadsm4++){
-			TH1D* hAmpForSpecificID = hAmpCellId->ProjectionX(Form("hAmpForSpecificID"), listDeadSM4[ideadsm4], listDeadSM4[ideadsm4]);
-			Int_t entries = hAmpForSpecificID->GetEntries();
-			if(entries > 0) {
-				Printf("Error! %d entries found in Cell %d, run %s", entries, listDeadSM4[ideadsm4], currentRun.Data());
+		//..Loop though the SM to set which cells are bad
+		for(Int_t column=0;column<48;column++)
+		{
+			for(Int_t row=0;row<24;row++)
+			{
+				Int_t inRow=row;
+				Int_t inCol=column;
+				cellID=geom->GetAbsCellIdFromCellIndexes(iSM,inRow,inCol);
+				fCaloUtils->GetModuleNumberCellIndexesAbsCaloMap(cellID,0,inCol,inRow,trash,cellColumnAbs,cellRowAbs);
+				if(h[iSM]->GetBinContent(column,row)==2)
+				{
+					plot2D_Bad_OADB->SetBinContent(cellColumnAbs,cellRowAbs,1);
+				}
+				if(h[iSM]->GetBinContent(column,row)==1)
+				{
+					plot2D_Dead_OADB->SetBinContent(cellColumnAbs,cellRowAbs,1);
+				}
 			}
-
-			delete hAmpForSpecificID;
 		}
-
-		for(Int_t ideadsm7 = 0; ideadsm7 < ndeadSM7; ideadsm7++){
-			TH1D* hAmpForSpecificID = hAmpCellId->ProjectionX(Form("hAmpForSpecificID"), listDeadSM7[ideadsm7], listDeadSM7[ideadsm7]);
-			Int_t entries = hAmpForSpecificID->GetEntries();
-			if(entries > 0) {
-				Printf("Error! %d entries found in Cell %d, run %s", entries, listDeadSM7[ideadsm7], currentRun.Data());
-			}
-			delete hAmpForSpecificID;
-		}
-
-		nruns++;
-
 	}
+
+	//..................................................................
+	TCanvas* C2 = new TCanvas("Info from OADB","Info from OADB",1);
+	C2->Divide(2);
+	C2->cd(1);
+	plot2D_Bad_OADB->DrawCopy("colz");
+	C2->cd(2);
+	plot2D_Dead_OADB->DrawCopy("colz");
+	//..................................................................
+	TCanvas* C3 = new TCanvas("Orig Maps from Analyis","Orig Maps from Analyis",1);
+	C3->Divide(2);
+	C3->cd(1);
+	h2DChannelMap_FlagBad->DrawCopy("colz");
+	C3->cd(2);
+	h2DChannelMap_FlagDead->DrawCopy("colz");
+	//..................................................................
+	TCanvas* C4 = new TCanvas("Division of OADB/Orig.","Division of OADB/Orig.",1);
+	C4->Divide(2);
+	C4->cd(1);
+	plot2D_Bad_OADB->Add(h2DChannelMap_FlagBad,-1);
+	plot2D_Bad_OADB->DrawCopy("colz");
+	C4->cd(2);
+	plot2D_Dead_OADB->Add(h2DChannelMap_FlagDead,-1);
+	plot2D_Dead_OADB->DrawCopy("colz");
 }
 //________________________________________________________________________
 void SummarizeRunByRun(TString period = "LHC15f", TString pass = "pass2",TString workDir=".", TString listName="runList.txt")
@@ -285,5 +337,73 @@ void SummarizeRunByRun(TString period = "LHC15f", TString pass = "pass2",TString
 			text3->SetTextColor(1);
 			text3->Draw();
 		}
+	}
+}
+//________________________________________________________________________
+void CheckListDeadChannels(TString qaoutputPath = "/data/Work/EMCAL/BadChannels/LHC16h/510muon_caloLego/", TString runlist = "/data/Work/EMCAL/BadChannels/LHC16h/rulListLHC16h_EMCGood_Fieldp30.txt", TString listname = "CaloQA_AnyINT", TString hname = "EMCAL_hAmpId")
+{
+
+	// current list of dead cells from Martin
+	const Int_t ndeadSM4 = 32;
+	Int_t listDeadSM4[ndeadSM4] = {4833, 4832, 4881, 4880, 4835, 4834, 4883, 4882, 4837, 4836, 4885, 4884, 4839, 4838, 4887, 4886, 4841, 4840, 4889, 4888, 4843, 4842, 4891, 4890, 4845, 4844, 4893, 4892, 4847, 4846, 4895, 4894};
+
+	const Int_t ndeadSM7 = 32;
+	Int_t listDeadSM7[ndeadSM7] = {8481, 8480, 8529, 8528, 8483, 8482, 8531, 8530, 8485, 8484, 8533, 8532, 8487, 8486, 8535, 8534, 8489, 8488, 8537, 8536, 8491, 8490, 8539, 8538, 8493, 8492, 8541, 8540, 8495, 8494, 8543, 8542};
+
+	ifstream read(runlist.Data());
+	Int_t nruns = 0;
+	TString currentRun = "";
+	while(read){
+		read>> currentRun;
+		Printf("Run %s", currentRun.Data());
+
+		TFile *fin = new TFile(Form("%s%s.root", qaoutputPath.Data(), currentRun.Data()));
+
+		if(!fin->IsOpen()){
+			Printf("%s%s.root not found", qaoutputPath.Data(), currentRun.Data());
+			continue;
+		}
+
+		TDirectoryFile *dir = (TDirectoryFile*)fin->Get(listname);
+		if(!dir){
+			Printf("%s not found", listname.Data());
+			fin->ls();
+			continue;
+		}
+
+		TList *list = (TList*)dir->Get(listname);
+		if(!list){
+			Printf("%s not found", listname.Data());
+			dir->ls();
+			continue;
+		}
+
+		TH2D *hAmpCellId = (TH2D*)list->FindObject(hname);
+		if(!hAmpCellId){
+			Printf("hAmpCellId not found");
+			continue;
+		}
+
+		for(Int_t ideadsm4 = 0; ideadsm4 < ndeadSM4; ideadsm4++){
+			TH1D* hAmpForSpecificID = hAmpCellId->ProjectionX(Form("hAmpForSpecificID"), listDeadSM4[ideadsm4], listDeadSM4[ideadsm4]);
+			Int_t entries = hAmpForSpecificID->GetEntries();
+			if(entries > 0) {
+				Printf("Error! %d entries found in Cell %d, run %s", entries, listDeadSM4[ideadsm4], currentRun.Data());
+			}
+
+			delete hAmpForSpecificID;
+		}
+
+		for(Int_t ideadsm7 = 0; ideadsm7 < ndeadSM7; ideadsm7++){
+			TH1D* hAmpForSpecificID = hAmpCellId->ProjectionX(Form("hAmpForSpecificID"), listDeadSM7[ideadsm7], listDeadSM7[ideadsm7]);
+			Int_t entries = hAmpForSpecificID->GetEntries();
+			if(entries > 0) {
+				Printf("Error! %d entries found in Cell %d, run %s", entries, listDeadSM7[ideadsm7], currentRun.Data());
+			}
+			delete hAmpForSpecificID;
+		}
+
+		nruns++;
+
 	}
 }
